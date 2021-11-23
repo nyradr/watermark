5afa52ea9fd5d5694c305b46c09c99de8671d66c
nyradr <nyradr@protonmail.com>
2021-11-23 20:06:09 +0100

    Display message when a non valid pdf is uploaded

--- src/watermark/display.js
+++ src/watermark/display.js
@@ -3,10 +3,20 @@ import React from "react";
 
 export class Display extends React.Component{
     render() {
-        return (
-            <div class="container fluid w-100 h-100">
-                <iframe class="w-100 h-100" src={ this.props.pdf } title="PDF preview"></iframe>
-            </div>
-        );
+        if (this.props.pdf.length == 0) {
+            return (
+                <div class="container fluid w-100 h-100">
+                    <div class="alert alert-danger mt-5" role="alert">
+                        Please upload a valid PDF file.
+                    </div>
+                </div>
+            );
+        } else {
+            return (
+                <div class="container fluid w-100 h-100">
+                    <iframe class="w-100 h-100" src={ this.props.pdf } title="PDF preview"></iframe>
+                </div>
+            );
+        }
     }
 }
