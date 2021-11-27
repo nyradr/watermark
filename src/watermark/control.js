81ed6448097a96291279f8b0afafd29b5557919a
nyradr <nyradr@protonmail.com>
2021-11-27 19:06:16 +0100

    line helpers

--- src/watermark/control.js
+++ src/watermark/control.js
@@ -22,48 +22,46 @@ export class Control extends React.Component {
                 <UploadPDF on_file_upload={ this.props.on_file_upload }></UploadPDF>
                 
                 <WSlider value = { this.props.repets }
-                        on_change = { this.props.on_repet_change }
-                        min="1" max="42" step="1"
-                        text="Number of repetitions"
-                        help_title="Repetitions"
-                        help_content="
+                         on_change = { this.props.on_repet_change }
+                         min="1" max="42" step="1"
+                         text="Number of repetitions"
+                         help_title="Repetitions"
+                         help_content="
                             Number of times to repet the watermark per document pages.
                             The watermark will be evenly distributed on the page.
-                        "
+                         "
                 ></WSlider>
 
                 <WSlider value = { this.props.rotation }
-                        on_change = { this.props.on_rotation_change }
-                        min="0" max="359" step="1"
-                        text="Watermark rotation"
-                        help_title="Rotation"
-                        help_content="
+                         on_change = { this.props.on_rotation_change }
+                         min="0" max="359" step="1"
+                         text="Watermark rotation"
+                         help_title="Rotation"
+                         help_content="
                             Image rotation in degrese.
                             0 degree means the watermark will be horizontally aligned from left to right.
                             180 degree means the watermark will be horizontally aligned from rigt to left (with downward text).
-                        "
+                         "
                 ></WSlider>
 
                 <WSlider value = { this.props.opacity }
-                        on_change = { this.props.on_opacity_change }
-                        min="1" max="100" step="1"
-                        text="Watermark opacity"
-                        help_title="Opacity"
-                        help_content="
+                         on_change = { this.props.on_opacity_change }
+                         min="1" max="100" step="1"
+                         text="Watermark opacity"
+                         help_title="Opacity"
+                         help_content="
                             Change the watermark opacity.
                             100 means the watermark will be totally opac and 0 means the watermark will be totally transparent.
-                        "
+                         "
                 ></WSlider>
 
                 <div class="d-flex flex-row justify-content-end">
-                    <button class="btn btn-link btn-sm" onClick={ this.props.on_line_new }>
-                        <small>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
-                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                                <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
-                            </svg>
-                        </small>
-                    </button>
+                    <small class="text-primary" role="button" onClick={ this.props.on_line_new }>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
+                        </svg>
+                    </small>
                 </div>
 
                 <div>
--- src/watermark/wline.js
+++ src/watermark/wline.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Form } from "react-bootstrap";
+import { Form, Popover } from "react-bootstrap";
+import { Helper } from "./helper";
 
 export class WLine extends React.Component {
     constructor(props){
@@ -43,18 +44,50 @@ export class WLine extends React.Component {
     }
 
     render() {
+        const helper_text = (
+            <Popover id="popover-basic">
+                <Popover.Header as="h3">Watermark text</Popover.Header>
+                <Popover.Body>
+                    Text that will be displayed.
+                </Popover.Body>
+            </Popover>
+        );
+
+        const helper_size = (
+            <Popover id="popover-basic">
+                <Popover.Header as="h3">Text size</Popover.Header>
+                <Popover.Body>
+                    Text size.
+                </Popover.Body>
+            </Popover>
+        );
+
         return (
             <div class="d-flex flex-row w-100 mt-2">
                 <div>
                     <Form.Group>
-                        <Form.Label>Watermark text</Form.Label>
+                        <div class="d-flex flex-row">
+                            <Form.Label>Watermark text</Form.Label>
+
+                            <div class="ms-1">
+                                <Helper helper = { helper_text }></Helper>
+                            </div>
+                        </div>
+
                         <Form.Control type="text" value={this.props.line.text}  onChange={ this.handle_text_change }></Form.Control>
                     </Form.Group>
                 </div>
 
                 <div>
                    <Form.Group>
-                        <Form.Label>Font size</Form.Label>
+                        <div class="d-flex flex-row w-100">
+                            <Form.Label>Font size</Form.Label>
+
+                            <div class="ms-1">
+                                <Helper helper = { helper_size }></Helper>
+                            </div>
+                        </div>
+                        
                         <Form.Select value={this.props.line.size} onChange={ this.handle_size_change }>
                             <option value="8">8</option>
                             <option value="10">10</option>
@@ -69,12 +102,12 @@ export class WLine extends React.Component {
                 </div>
 
                 <div class="ms-auto">
-                    <button class="btn text-danger btn-sm" onClick={ this.handle_delete }>
+                    <a class="text-danger" role="button" onClick={ this.handle_delete }>
                         <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                             <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
                             <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
                         </svg>
-                    </button>
+                    </a>
                 </div>
 
                 
